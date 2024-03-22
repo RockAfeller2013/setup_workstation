937ac26659755447cf571de110b6bf68d353fcb7
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2024-03-22 14:41:56 +1000

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -17,7 +17,8 @@ sc config "wuauserv" start=disabled
 @echo Finished ....
 pause
 
-
+# Disable firewall
+netsh advfirewall set allprofiles state off
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install googlechrome --ignore-checksums --force -y
