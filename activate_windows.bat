abdf06d4cd8c8048794d767b70feed721a1e18e4
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-02-24 15:34:20 +1000

    Create activate_windows.bat

---
+++ activate_windows.bat
@@ -0,0 +1,20 @@
+@echo off
+
+winrm set winrm/config/service @{AllowUnencrypted="true"}
+winrm set winrm/config/service/auth @{Basic="true"}
+
+echo Activating Windows...
+slmgr /ipk $WIN_LICENSE_KEY
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Activation complete.
+
+winrm quickconfig
+
+net user vagrant vagrant /add
+net localgroup Administrators vagrant /add
+
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+shutdown /s /t 0
+
