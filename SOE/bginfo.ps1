7be1b3c47337c0b1515d912458d2436574195c42
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-04 12:09:47 +1000

    Update bginfo.ps1

--- SOE/bginfo.ps1
+++ SOE/bginfo.ps1
@@ -1,4 +1,8 @@
 & "C:\ProgramData\chocolatey\bin\Bginfo64.exe"  /accepteula /silent /timer:0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" `
+  -Name "BgInfo" `
+  -Value '"C:\ProgramData\chocolatey\bin\Bginfo64.exe" /accepteula /silent /timer:0'
+
 
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
