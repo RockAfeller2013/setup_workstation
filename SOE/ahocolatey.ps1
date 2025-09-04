46d66047e9680ab9eb63d18c95c7245cc68cfb64
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-04 18:52:51 +1000

    Update ahocolatey.ps1

--- SOE/ahocolatey.ps1
+++ SOE/ahocolatey.ps1
@@ -2,9 +2,7 @@
 # --- Applications via Chocolatey with logging ---
 
 # --- Chocolatey bootstrap ---
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey -y
 
 <#
