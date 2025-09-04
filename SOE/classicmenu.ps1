a8fe8b28538227c6745c62e6a7fac78301bde77b
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-04 12:17:37 +1000

    Create classicmenu.ps1

---
+++ SOE/classicmenu.ps1
@@ -0,0 +1,25 @@
+@ "C:\Program Files\Classic Shell\ClassicStartMenu.exe" -exit
+# @ "C:\Program Files\Classic Shell\ClassicStartMenu.exe" -settings
+
+
+# Source values from current user
+$src = "HKCU:\Software\IvoSoft"
+
+# Apply to default profile (for new users)
+$dstDefault = "HKU:\.DEFAULT\Software\IvoSoft"
+if (-not (Test-Path $dstDefault)) {
+    New-Item -Path $dstDefault -Force | Out-Null
+}
+Copy-Item -Path $src\* -Destination $dstDefault -Recurse -Force
+
+# Apply to each existing user hive
+Get-ChildItem "HKU:\" | Where-Object { $_.Name -match '^HKEY_USERS\\S-1-5-21' } | ForEach-Object {
+    $dst = "$($_.PSPath)\Software\IvoSoft"
+    if (-not (Test-Path $dst)) {
+        New-Item -Path $dst -Force | Out-Null
+    }
+    Copy-Item -Path $src\* -Destination $dst -Recurse -Force
+}
+
+
+@ "C:\Program Files\Classic Shell\ClassicStartMenu.exe"
