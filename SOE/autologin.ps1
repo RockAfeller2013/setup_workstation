ebfc58cf5524d4aae686f0c4cd4a51fa3e857144
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-05 19:34:51 +1000

    Delete SOE/autologin.ps1

--- SOE/autologin.ps1
+++
@@ -1,7 +0,0 @@
-# --- Auto logon (set your own credentials) ---
-$User = "vagrant"
-$Pass = "vagrant"
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $User
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $Pass
-Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogonCount -ErrorAction SilentlyContinue
