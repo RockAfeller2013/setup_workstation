92f5a7fa549a9eaa23bfbd83bf502a8ccbafe14e
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-05 18:10:23 +1000

    Update config.ps1

--- SOE/config.ps1
+++ SOE/config.ps1
@@ -134,35 +134,6 @@ Write-Output  "Ensure there is a profile file so we can get tab completion"
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 Write-Output "Powershell Memory"
 
-# Set Preferences
-$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer'
-$advancedKey = "$key\Advanced"
-Set-ItemProperty $advancedKey Hidden 1
-Set-ItemProperty $advancedKey HideFileExt 0
-Set-ItemProperty $advancedKey ShowSuperHidden 1
-
-$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$parts = $identity.Name -split "\\"
-$user = @{Domain = $parts[0]; Name = $parts[1]}
-
-try {
-    try { $explorer = Get-Process -Name explorer -ErrorAction stop -IncludeUserName }
-    catch {$global:error.RemoveAt(0)}
-
-    if ($explorer -ne $null) {
-        $explorer | ? { $_.UserName -eq "$($user.Domain)\$($user.Name)"} | Stop-Process -Force -ErrorAction Stop | Out-Null
-    }
-
-    Start-Sleep 1
-
-    if (!(Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
-        $global:error.RemoveAt(0)
-        start-Process -FilePath explorer
-    }
-}
-catch {$global:error.RemoveAt(0)}
-Write-Output "Set Preferences"
-
 # Enable Network Discovery and File and Print Sharing
 # Taken from here: http://win10server2016.com/enable-network-discovery-in-windows-10-creator-edition-without-using-the-netsh-command-in-powershell
 Write-Host "Enabling Network Discovery."
@@ -184,7 +155,7 @@ if ($osData.ProductType -eq 3) {
     Write-Host 'Disabling Server Manager for starting at login.'
     Get-ScheduledTask -TaskName 'ServerManager' | Disable-ScheduledTask | Out-Null
 }
-Write-Outpu "Disable Server message"
+Write-Output "Disable Server message"
 
 
 Write-Output "System setup completed. Reboot recommended."
