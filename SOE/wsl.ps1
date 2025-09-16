985118a8fe776160df62561c18e4eb28dd015c53
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-16 12:51:52 +1000

    Update wsl.ps1

--- SOE/wsl.ps1
+++ SOE/wsl.ps1
@@ -7,23 +7,29 @@
 
 # Prevent Windows from automatically installing Hyper-V features
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 1 /f
+
 # Use PowerShell to update WSL components selectively
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
+wsl.exe --install Ubuntu
+wsl.exe -d Ubuntu
 
 # Convert existing distributions to WSL1
-wsl --set-version <DistributionName> 1
+wsl --set-version Ubuntu 1
 
 # Set WSL1 as default
+# WSL1 uses less resources and doesn't require Hyper-V virtualization
+# Some features are different - WSL1 uses a translation layer instead of a real VM
 wsl --set-default-version 1
 
 
-wsl --list --verbose
-wsl --status
+
 
 # Create .wslconfig file in your user directory with:
 [wsl2]
 kernelCommandLine = noapic
+guiApplications = false
+nestedVirtualization = false
 
 # Remove Hyper-V components
 dism /Online /Disable-Feature:Microsoft-Hyper-V /All
@@ -35,4 +41,7 @@ systeminfo | find "Hyper-V"
 
 
 wsl.exe --list --online
-wsl.exe --install Debian
+wsl.exe --list --verbose
+wsl.exe --status
+
+
