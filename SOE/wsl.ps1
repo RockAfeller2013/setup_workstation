c67e501c80501e2ee44d51bad462f5e0a5177ba8
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-16 12:42:11 +1000

    Update wsl.ps1

--- SOE/wsl.ps1
+++ SOE/wsl.ps1
@@ -10,9 +10,29 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDr
 # Use PowerShell to update WSL components selectively
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
+
+# Convert existing distributions to WSL1
+wsl --set-version <DistributionName> 1
+
+# Set WSL1 as default
+wsl --set-default-version 1
+
+
 wsl --list --verbose
 wsl --status
 
+# Create .wslconfig file in your user directory with:
+[wsl2]
+kernelCommandLine = noapic
+
 # Remove Hyper-V components
 dism /Online /Disable-Feature:Microsoft-Hyper-V /All
+dism /Online /Disable-Feature:Microsoft-Hyper-V
+dism /Online /Disable-Feature:VirtualMachinePlatform
 bcdedit /set hypervisorlaunchtype off
+
+systeminfo | find "Hyper-V"
+
+
+wsl.exe --list --online
+wsl.exe --install Debian
